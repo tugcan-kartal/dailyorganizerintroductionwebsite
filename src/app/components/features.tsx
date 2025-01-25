e95b98dbafb67fea5d0bfb47da33560e4edc1fd0
tugcan-kartal <tugcankartal01@gmail.com>
2025-01-25 17:58:51 +0300

    added features part

---
+++ src/app/components/features.tsx
@@ -0,0 +1,74 @@
+import Image, { StaticImageData } from "next/image";
+import React from "react";
+import TaskAddPage from "@/../public/assets/taskAddPage.png";
+import TaskEditPart from "@/../public/assets/taskEditPart.png";
+import TaskFilterPart from "@/../public/assets/taskFilterPart.png";
+import GbtPage from "@/../public/assets/gbtPage.png";
+
+interface FeatureParagraphProps {
+  header: string;
+  seconder: string;
+  thirder: string;
+  fourthImg: StaticImageData;
+  reverse?: boolean;
+}
+
+const features=[
+    {
+        header: "Taskini ekle", 
+        seconder: "Yapman gerekenleri koyabilirsin", 
+        thirder: "Taskinin detaylarini dikkatlice belirt yapay zeka tarafindan tarancak daha sonra", 
+        fourthImg: TaskAddPage
+    },
+    {
+        header: "Taskini düzenleme", 
+        seconder: "Taskini sonradan düzenleme", 
+        thirder: "Taskini düzenleyebilirsin sonradan edit yaparak silmene gerek kalmadan siralayabilirsin ve yapildi olarak işaretleme", 
+        fourthImg: TaskEditPart
+    },
+    {
+        header: "Tasklarini filtreleme ve siralama", 
+        seconder: "Tasklarini önceliğine göre sirala", 
+        thirder: "Tasklarini kategorilerine önemlilik derecelerine tarihlerine ve bitip bitmemelerine göre ayarlayabilirsin", 
+        fourthImg: TaskFilterPart
+    },
+    {
+        header: "Taskini ai ile daniş", 
+        seconder: "Tasklarini ail ile sorgulatabilme", 
+        thirder: "Taskini yapay zeka ile ilk olarak tavsiye isteyebilirsin veya taskinla alakali sorularini sorabilirsin sen eklediğinde zaten otomatik tariyor", 
+        fourthImg: GbtPage
+    }
+]
+
+export const FeatureParagraph: React.FC<FeatureParagraphProps> = ({ header, seconder, thirder, fourthImg,reverse }) => {
+  return (
+    <div className={`flex justify-center items-center gap-x-[15%] my-[10%] ${reverse ? "flex-row-reverse" : "flex-row"}`}>
+        <div className="flex flex-col gap-y-[1vh] text-start my-[5%] w-[30%]">
+        <div className="text-blue-400">{header}</div>
+        <div className="text-gray-800 text-2xl">{seconder}</div>
+        <div className="text-xl text-gray-400">
+            {thirder}
+        </div>
+        </div>
+
+        <div className="w-[60%]">
+            <Image src={fourthImg} alt="not found"/>
+        </div>
+    </div>
+
+  );
+};
+
+const Features = () => {
+  return (
+    <div className="mt-[20vh]">
+      <div>
+        {features.map((feature,index)=>(
+            <FeatureParagraph key={index} header={feature.header} seconder={feature.seconder} thirder={feature.thirder} fourthImg={feature.fourthImg} reverse={index % 2 !== 0}/>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Features;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/navbar";
 import HeroSection from "./components/heroSection";
+import Features from "./components/features";
 
 export default function Home() {
   return (
     <div className="w-[80%] mx-auto">
       <Navbar />
       <HeroSection />
+      <Features />
     </div>
   );
 }
